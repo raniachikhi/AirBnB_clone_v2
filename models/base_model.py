527bf11d0103b821d025b632ddbdd59e70708079
raniachikhi <raniach0653@gmail.com>
2023-11-23 07:36:47 -0800

    add

--- models/base_model.py
+++ models/base_model.py
@@ -6,13 +6,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
-
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime(), default=datetime.utcnow(), nullable=False)
-    
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
